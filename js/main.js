e3852623aaa493b363fc6b078ba1f8912740743a
Nikhil Patel <n.patel@berkeley.edu>
2020-09-19 12:59:52 -0700

    Finishing touches, add some emojis

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const fromList = [
 	"January 21: The first case of COVID-19 in the US was reported",
 	"January 26: Kobe Bryant died in a helicopter crash",
 	"January: The US almost went to war with Iran",
+	"February 4: Rush Limbaugh was awarded the Presidential Medal of Freedom",
 	"February 5: Trump was acquitted",
 	"January 31: The UK officially withdrew from the EU",
 	"February 17: 300 Americans were evacuated from the Diamond Princess cruise ship",
@@ -12,7 +13,7 @@ const fromList = [
 	"March 8: Italy declared nationwide quarantine",
 	"March 11: The WHO declared COVID-19 a pandemic",
 	"March 16: The stock market had the largest fall in history: 📉",
-	"April 14: Trump suspends all US funding to the WHO",
+	"April 14: Trump suspended all US funding to the WHO",
 	"April 20: The price of oil became negative for the first time ever: 🛢️",
 	"April 27: The Pentagon released videos of UFOs: 🛸",
 	"April: There were rumors that Kim Jong Un died or was in a coma",
@@ -20,10 +21,8 @@ const fromList = [
 	"May 13: Paul Manafort was released to house arrest",
 	"May 21: Number of confirmed COVID-19 cases passed 5 million",
 	"May 25: George Floyd was killed by police",
-	"May 26: Costa Rica became the first Central American country to legalize same-sex marriage",
 	"May 26: Twitter labeled a tweet by Trump as misleading for the first time",
-	"May 27: China undermined Hong Kong's autonomy with new national security laws",
-	"May 30: SpaceX launched the first manned spacecraft from US soil since the Space Shuttle in 2011",
+	"May 30: SpaceX launched the first manned spacecraft from US soil since the Space Shuttle in 2011: 🚀",
 	"June 8: Protestors formed an autonomous zone in Seattle",
 	"June 15: The Supreme Court ruled that employers cannot discriminate based on sexual orientation",
 	"June 15: 60 soldiers were killed or injured in a border clash between India and China",
@@ -32,7 +31,6 @@ const fromList = [
 	"July 2: Ghislaine Maxwell was arrested",
 	"July 4: Kanye announced his presidential campaign",
 	"July 15: Twitter was hacked by a 17-year-old",
-	"July 30: NASA launched Mars 2020 rover",
 	"August 6: Trump signs an executive order banning TikTok and WeChat after 45 days",
 	"August 6: Mark Zuckerberg's net worth passed $100 billion",
 	"August 10: Number of confirmed COVID-19 cases passed 20 million",
@@ -40,7 +38,6 @@ const fromList = [
 	"August 13: The USPS tried to remove hundreds of mail sorting machines",
 	"August 16: A huge thunderstorm kicked off hundreds of wildfires in California",
 	"August 20: Steve Bannon was arrested for fraud over a fundraising campaign for the US-Mexico wall",
-	"August 25: Africa was declared free of wild polio",
 	"August 26: Jeff Bezos's net worth passed $200 billion (first person in history)",
 	"August 30: Number of confirmed COVID-19 cases passed 25 million",
 	"September 2: Trump told North Carolina voters to commit voter fraud by voting twice",
@@ -68,7 +65,7 @@ function shuffleInPlace(array) {
 
 function populateEvents() {
 	fromList.forEach(e => {
-		const [date, text, emoji] = e.split(': ')
+		const [date, text, emoji] = e.split(': ');
 
 		events.push({
 			date: date,
@@ -112,7 +109,6 @@ function spawnEvent() {
 	});
 
 	document.querySelector(".content").appendChild(eventElement);
-
 
 	/* Set event contents */
 	const eventContent = sampleEvent();
